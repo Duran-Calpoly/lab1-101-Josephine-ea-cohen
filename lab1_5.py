60715cf7b99a9b9949f8f9d59d219a2052dadc2f
Josephine-ea-cohen <jos3phine.cohen@gmail.com>
2026-01-17 03:02:46 +0000

    done

--- lab1_5.py
+++ lab1_5.py
@@ -1,1 +1,34 @@
-def
+def check_multiple (number):
+    return number % 3 == 0 and number % 5 == 0
+
+def check_password (input_string):
+    secret_word = "Python123"
+    if input_string = secret_word:
+        return "access granted"
+    else:
+        return "access denied"
+
+def calculate_federal_tax(salary):
+    """calculate federal tax based on salary brackets"""
+    if salary <= 1100:
+        tax_rate = 0.10
+        
+    elif salary <= 44725:
+        tax_rate = 0.12
+    elif salary <= 93575:
+        tax_rate = 0.22
+    elif salary <= 182100:
+        tax_rate = 0.24
+    else:
+        tax_rate = 0.32
+    
+    return salary * tax_rate
+
+
+
+
+
+
+
+#for password us Python123
+
